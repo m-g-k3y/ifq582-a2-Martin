90ca7bed5165c7a8ab20fe8aaee89e2524ca81a2
Kai-Bin Wong <wongkaibin8080@gmail.com>
2026-06-13 11:46:06 +1000

    switched to BS5, added some folder structure and some py files to start

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    
+    app.config.from_pyfile('config.py')
+    
+    from .routes import main_bp
+    app.register_blueprint(main_bp)
+    
+    return app
---
+++ app/routes.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template
+
+main_bp = Blueprint('main', __name__)
+
+@main_bp.route('/')
+def index():
+    return render_template('index.html')
+
+@main_bp.route('/about')
+def about():
+    return render_template('about.html')
+
+@main_bp.route('/item/<int:item_id')
+def item_detail(item_id):
+    return render_template('item_details.html', id=item_id)
